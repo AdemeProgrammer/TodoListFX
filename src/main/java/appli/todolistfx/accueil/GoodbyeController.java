50c5b04ce3703e488d4597bdebd609fcd539e3ce
Ademe <A.AYAD@lprs.fr>
2025-03-06 15:24:27 +0100

    [fix] Reorganisation des fichiers

---
+++ src/main/java/appli/todolistfx/accueil/GoodbyeController.java
@@ -0,0 +1,14 @@
+package appli.todolistfx;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class GoodbyeController {
+    @FXML
+    private Label goodbyeText;
+
+    @FXML
+    protected void onGoodbyeButtonClick() {
+        goodbyeText.setText("Goodbye la team !");
+    }
+}
---
+++ src/main/java/appli/todolistfx/accueil/InscriptionController.java
@@ -0,0 +1,15 @@
+package appli.todolistfx.accueil;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class InscriptionController extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        
+    }
+}
