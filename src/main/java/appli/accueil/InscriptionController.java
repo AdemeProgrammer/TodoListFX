cc27e00f0ead1db116c4f1a8abcdcd564d0b1e17
Ademe <A.AYAD@lprs.fr>
2025-03-11 10:01:57 +0100

    [feat] Mise en place de l'inscription

--- src/main/java/appli/accueil/InscriptionController.java
+++ src/main/java/appli/accueil/InscriptionController.java
@@ -1,8 +1,13 @@
 package appli.accueil;
 
+import appli.StartApplication;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+
+import java.io.IOException;
 
 public class InscriptionController {
 
@@ -16,6 +21,28 @@ public class InscriptionController {
     private TextField emailField;
 
     @FXML
+    private Label error;
+
+    @FXML
     private TextField motDePasseField;
 
+    void onInscriptionButtonClick (ActionEvent event) throws IOException {
+        StartApplication.changeScene("accueil/inscription");
+    }
+
+    @FXML
+    void onLoginButtonClick(ActionEvent event) {
+        System.out.println("Email"+ emailField.getText());
+        System.out.println("Password"+ motDePasseField.getText());
+        String email = emailField.getText();
+        String password =  motDePasseField.getText();
+        if(email.isEmpty() || password.isEmpty()) {
+            error.setText("Veuillez remplir tous les champs");
+        }else if (!password.isEmpty() && !email.isEmpty()){
+            error.setText("Inscription réussie");
+        }else{
+            error.setText("Il y a un problème quelque part");
+        }
+
+    }
 }
