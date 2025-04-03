6fc788ed791282251d001270c65a5ee4a6a5064b
Ademe <A.AYAD@lprs.fr>
2025-04-03 13:55:55 +0200

    [feat] SessionUtilisateur
    TP3 : la sessionUtilisateur a été créée et est normalement fonctionnelle.

--- src/main/java/session/SessionUtilisateur.java
+++ src/main/java/session/SessionUtilisateur.java
@@ -5,9 +5,7 @@ import model.Utilisateur;
 public class SessionUtilisateur {
     private static SessionUtilisateur instance;
     private Utilisateur utilisateurConnecte;
-
     private SessionUtilisateur() { }
-
     public static SessionUtilisateur getInstance() {
         if (instance == null) {
             instance = new SessionUtilisateur();
@@ -15,17 +13,16 @@ public class SessionUtilisateur {
         return instance;
     }
     public void sauvegardeSession(Utilisateur utilisateur) {
-        if (this.utilisateurConnecte == null) {
+        if (this.utilisateurConnecte != null) {
             this.utilisateurConnecte = utilisateur;
         }
     }
-
     public Utilisateur getUtilisateur() {
         return utilisateurConnecte;
     }
-
     public void deconnecter() {
         utilisateurConnecte = null;
     }
 }
+
 
